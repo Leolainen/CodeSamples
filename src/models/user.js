bd39aeb232b6b057cc5d5a962301c929f271be57
Leolainen <leolainen2@gmail.com>
2019-01-17 18:26:06 +0100

    passwords are now hashed when new users are created using bcryptjs

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { hash } from "bcryptjs";
 
 /**
  * user object generally has a "createdAt" key.
@@ -21,6 +22,19 @@ const userSchema = new mongoose.Schema(
     timestamps: true
   }
 );
+
+// Hash the password before a user is saved in the database
+// needs to be a regular function so that "this" refers to the user being worked on
+userSchema.pre("save", async function(next) {
+  if (this.isModified("password")) {
+    try {
+      this.password = await hash(this.password, 10);
+    } catch (err) {
+      next(err);
+    }
+  }
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
