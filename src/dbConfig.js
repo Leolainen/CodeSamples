2aa897cc1e5fd83b7d8c109c304f8fc0f21aadf6
Leolainen <leolainen2@gmail.com>
2019-01-21 18:05:50 +0100

    changed information in dbConfig to backup. - IMPORTANT: when uploading this project to github create a NEW project with no previous git history.

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -1,7 +1,7 @@
 export const {
   DB_USERNAME = "admin",
-  DB_PASSWORD = "wzNaTify4x3GeNAkzf",
-  DB_HOST = "ds159634.mlab.com",
+  DB_PASSWORD = "secret",
+  DB_HOST = "localhost",
   DB_PORT = "59634",
   DB_NAME = "codesamples"
 } = process.env;
