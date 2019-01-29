845934ccf3cc170bbe1b95d4033d7641f4468482
Leolainen <leolainen2@gmail.com>
2019-01-30 00:41:54 +0100

    Worked on Button component. - Mostly updated styling - at the moment the onClick function only applies a class that paints the button red

--- client/components/Button/index.js
+++ client/components/Button/index.js
@@ -4,23 +4,24 @@ import classnames from "classnames";
 
 import { WAS_CLICKED } from "./constants";
 import reducer from "./reducer";
-
 import styles from "./style.scss";
 
 const initialState = {
   wasClicked: false
 };
 
-export default function Button({ children, ...rest }) {
+export default function Button({ rounded, children, ...rest }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const style = classnames(styles.button, {
-    [styles.wasClicked]: state.wasClicked
+    [styles.wasClicked]: state.wasClicked, // for testing
+    [styles.rounded]: rounded
   });
 
   return (
     <button
       className={style}
+      type="button"
       {...rest}
       onClick={() => dispatch({ type: WAS_CLICKED })}
     >
@@ -28,34 +29,6 @@ export default function Button({ children, ...rest }) {
     </button>
   );
 }
-
-// const Button = props => {
-//   const store = useContext(Context);
-
-//   const style = classnames({
-//     [styles.header]: store.wasClicked
-//   });
-
-//   const handleClick = () => {
-//     store.dispatch({ type: "WAS_CLICKED" });
-//   };
-
-//   return (
-//     <div className={styles.example}>
-//       <h1 className={style}>
-//         {store.wasClicked ? "Was clicked" : "was NOT clicked"}!
-//       </h1>
-
-//       <button
-//         className={classnames({ [styles.wasClicked]: store.wasClicked })}
-//         onClick={handleClick}
-//         type="button"
-//       >
-//         {props.children}
-//       </button>
-//     </div>
-//   );
-// };
 
 Button.propTypes = {
   children: PropTypes.node
--- client/components/Button/story.js
+++ client/components/Button/story.js
@@ -1,4 +1,5 @@
 import { action } from "@storybook/addon-actions";
+import { boolean } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 
@@ -7,7 +8,12 @@ import Button from "./index";
 const stories = storiesOf("Button", module);
 
 stories.add("with text", () => (
-  <Button onClick={action("Button was clicked")}>Hello Button</Button>
+  <Button
+    rounded={boolean("Rounded", false)}
+    onClick={action("Button was clicked")}
+  >
+    Hello Button
+  </Button>
 ));
 
 stories.add("with some emoji", () => (
